9542fb4767baf5bb0caccbdc24f9c625d3983047
kkeomalaythong <kkeomalaythong117@gmail.com>
2018-11-01 15:15:21 -0400

    Modified database code to utilize PostgreSQL database server

---
+++ src/database/batcall.py
@@ -0,0 +1,10 @@
+# coding: utf-8
+class batcall:
+    def __init__(self, dots, metadata, label):
+        self.dots = dots
+        self.metadata = metadata
+        self.label = label
+        
+    dots = [[0, 0]]
+    metadata = 'null'
+    label = 'unlabeled'
