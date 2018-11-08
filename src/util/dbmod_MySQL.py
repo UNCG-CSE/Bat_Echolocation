c992f0396f1e46558ae3aafaa5242a871a35a0a9
kkeomalaythong <kkeomalaythong117@gmail.com>
2018-11-08 17:38:24 -0500

    Further updates to database code and further code testing

--- src/util/dbmod_MySQL.py
+++ src/util/dbmod_MySQL.py
@@ -2,7 +2,7 @@
 The following code will require a local MySQL server to be currently running on your machine,
 otherwise it will trigger an OperationalError when importing this module.
 This can be done with either Wampserver for Windows systems, MAMP for macOS systems, or LAMP for Linux systems.
-Just have the MySQL server running when you have either one of them installed.
+Just have the MySQL server running when you have either one of them installed to avoid any errors.
 
 Then, when the above is done, you should have your preset hostname as localhost and username as root,
 with the password initialized as blank. Use these login credentials for the connect_to_db() function.
@@ -17,164 +17,178 @@ Also, I will be testing this code in the db_kevin Jupyter notebook using sample 
 
 import mysql.connector
 
-db_conn = None
-c = None
-
-"""
-START: code to be evaluated later
-"""
+hostname, username, password = None, None, None
+db_connection, db_cursor = None, None
+curr_db = None
+db_tables = None
 
 
 def connect_to_db(db_host, db_user, db_pwd, db_name):
-    global db_conn, c
+    global hostname, username, password
+    global db_connection, db_cursor
+    global curr_db, db_tables
+
     try:
+        db_connection = mysql.connector.connect(
+            host=db_host,
+            user=db_user,
+            passwd=db_pwd
+        )
+        hostname, username, password = db_host, db_user, db_pwd
+
+        db_cursor = db_connection.cursor()
+
         if db_name is not None:
-            db_conn = mysql.connector.connect(
+            curr_db = db_name
+            db_connection = mysql.connector.connect(
                 host=db_host,
                 user=db_user,
                 passwd=db_pwd,
-                database = db_name
+                database=db_name
             )
-        else:
-            db_conn = mysql.connector.connect(
-                host=db_host,
-                user=db_user,
-                passwd=db_pwd
-            )
+            db_cursor = db_connection.cursor()
 
-        c = db_conn.cursor()
+            """ Python 3 """
+            print(f'Database access granted - current database is {curr_db}')
 
-    except:
-        print('Unable to establish connection.'
-              '\nPlease check whether service is running, then reconnect.')
+            """ Python 2 """
+            # print('Database access granted - current database is {}'.format(curr_db))
+
+        server_info()
+    except mysql.connector.errors.InterfaceError:
+        # error with hostname and/or whether service is online
+        print('Unable to establish connection - check whether the status is online '
+              'and whether the hostname is correct, then reconnect.')
+    except mysql.connector.errors.ProgrammingError:
+        # error w/ one or a combination of username, pwd, and database name
+        print('Unable to verify credentials and/or database name - '
+              'please correct any errors in either field before reconnecting.')
 
 
-def create_database(db_name):
-    global db_conn, c
+def server_info():
+    global curr_db, db_tables
+
+    """ Python 3 """
+    print(f'Current database: {curr_db}\nCurrent tables in {curr_db}: {db_tables}')
+
+    """ Python 2 """
+    # print('Current database: {}\nCurrent tables in {}: {}'.format(curr_db, curr_db, db_tables))
+
+
+def switch_database(db_name):
+    global db_cursor
     try:
-        c.execute('CREATE DATABASE :database;',
-                  {'database': db_name})
-    except:
-        print(f'Database \"{db_name}\" already exists.'
-              '\nEither create one with a different name or modify this one.')
+        db_cursor.execute('CREATE DATABASE %s;', (db_name,))
+    except mysql.connector.errors.ProgrammingError:
+        """ Python 3 """
+        print(f'Database \"{db_name}\" already exists.')
+
+        """ Python 2 """
+        # print('Database \"{}\" already exists.'.format(db_name))
 
 
-"""
-END: code to be evaluated later
-"""
+def access_database(db_name):
+    global hostname, username, password, db_tables, curr_db
+    connect_to_db(hostname, username, password, db_name)
+    get_tables_from_db(curr_db)
+
+
+def drop_table(tbl_name):
+    global db_cursor, db_tables
+
+    try:
+        db_cursor.execute('DROP TABLE %s;', (tbl_name,))
+        db_tables.remove(tbl_name)
+    except mysql.connector.errors.ProgrammingError:
+        """ Python 3 """
+        print(f'Table {tbl_name} does not exist in current database')
+
+        """ Python 2 """
+        # print('Table {tbl_name} does not exist in current database'.format(tbl_name))
+
 
 """
 According to Hadi, the data is essentially a set of points, each having corresponding x and y values; 
 x represents the time values, and y represents the frequency values. 
 The following create_table() function will create a new table with two columns: one for the x values, 
 and another for the y values. I am considering including another column for cluster identification for a later version.
+
+I'm debating on whether to create a table with dynamic number of columns rather than hard-coding just 2 (or 3) columns.
+As of the moment I'll settle on the latter.
 """
 
 
 def create_table(tbl_name):
-    global c
-    try:
-        c.execute('CREATE TABLE :table (time REAL, frequency REAL);',
-                  {'table': tbl_name})
-        print(f'Table \"{tbl_name}\" has been created successfully.')
-        show_tables()
-    except:
-        print(f'Table \"{tbl_name}\" already exists.'
-              '\nEither create one with a different name or modify this one.')
+    global curr_db
 
+    db_cursor.execute('CREATE TABLE %s (time REAL, frequency REAL);', (tbl_name,))
+
+    """ Python 3 """
+    print(f'Table \"{tbl_name}\" has been created successfully.')
+
+    """ Python 2 """
+    # print('Table \"%s\" has been created successfully.', (tbl_name,))
+
+    get_tables_from_db(curr_db)
+
+
+def get_tables_from_db(db_name):
+    global db_cursor, db_tables
+
+    db_cursor.execute('SHOW TABLES;')
+    tmp = db_cursor.fetchall()
+    db_tables = [tmp[i][0] for i in range(0, len(tmp))]
+
+    """ Python 3 """
+    print(f'Current tables in {db_name}: {db_tables}')
+
+    """ Python 2 """
+    # print('Current tables in {}: {}'.format(db_name, db_tables))
+
+
+"""
+Debating whether to keep this function:
 
 def show_tables():
-    global c
-    c.execute('SHOW TABLES;')
-    tmp = c.fetchall()
-    tables = [c.fetchall()[i][0] for i in range(0, len(c.fetchall()))]
-    print(tables)
+    global db_tables
+    print('Current tables: %s', (db_tables,))
+"""
 
 
-def select_from_table(tbl_name):
-    global c
+def show_specific_table(tbl_name):
+    global db_cursor
     try:
-        c.execute('SELECT * FROM :table',
-                  {'table': tbl_name})
-    except:
-        print(f'Table {tbl_name} does not exist in current database.'
-              '\nEither create this table or select from a different table.')
+        db_cursor.execute('SELECT * FROM %s', (tbl_name,))
+    except mysql.connector.errors.ProgrammingError:
+        """ Python 3 """
+        print(f'Table {tbl_name} does not exist in current database.')
+
+        """ Python 2 """
+        # print('Table {} does not exist in current database.'.format(tbl_name))
 
 
-def insert_into_table(tbl_name, time_val, frequency_val):
-    global db_conn, c
-    with db_conn:
-        c.execute('INSERT INTO :table VALUES (:x, :y)',
-                  {'x': time_val, 'y': frequency_val})
-    select_from_table(tbl_name)
+"""
+Below function insert_into_table() inserts values into a 2-column table according to current create_table() function.
+Will be changed if create_table() has a dynamic number of columns.
+"""
+
+
+# below function inserts values into a 2-column table
+def insert_into_table(tbl_name, val_1, val_2):
+    global db_connection, db_cursor
+
+    db_cursor.execute('INSERT INTO %s VALUES (%s, %s);', (tbl_name, val_1, val_2,))
+    db_connection.commit()
 
 
 def get_no_of_rows(tbl_name):
-    global c
+    global db_cursor
     try:
-        c.execute('SELECT COUNT(*) FROM :table;',
-                  {'table': tbl_name})
-    except:
-        print(f'Table {tbl_name} does not exist in current database.'
-              '\nEither create this table or select from a different table.')
+        db_cursor.execute('SELECT COUNT(*) FROM %s;', (tbl_name,))
+        # db_cursor.execute('SELECT COUNT(*) FROM :table;', {'table': tbl_name})
+    except mysql.connector.errors.ProgrammingError:
+        """ Python 3 """
+        print(f'Table {tbl_name} does not exist in current database.')
 
-
-def aggregate(tbl_name, tbl_col, agg_func):
-    global c
-
-    print(tbl_name)
-    select_from_table(tbl_name)
-
-    c.execute('SHOW COLUMNS FROM :table;',
-              {'table': tbl_name})
-    tmp = c.fetchall()
-    col_lst = [tmp[i][0] for i in range(0, len(tmp))]
-    if tbl_col not in col_lst:
-        print(f'Invalid option - please select a column in {tbl_name}')
-
-    if agg_func not in ['avg', 'min', 'max', 'sum', 'count']:
-        print('Invalid aggregation function')
-
-    c.execute('SELECT :function(:column) FROM :table;')
-    return c.fetchall()
-
-    # sql_cmd = ''
-
-    # ipt_1 = input('Select an aggregate function:'
-    #             '\n1: avg'
-    #             '\n2: min'
-    #             '\n3: max'
-    #             '\n4: sum'
-    #             '\n5: count'
-    #             '\n')
-
-    # while ipt_1 not in ['1', '2', '3', '4', '5',
-    #                   'average', 'min', 'max', 'sum', 'count']:
-    #     ipt_1 = input('Not a valid option - please try again.\n')
-    #
-    # ipt_2 = input('Select ')
-
-    # if ipt in ['1', 'avg']:
-    #     ipt = input('Select a column:'
-    #                 '\n1: \"x\" or \"time\"'
-    #                 '\n2: \"y\" or \"freq\" or \"frequency\"')
-    #
-    #     while ipt not in ['1', '2', 'x', 'y', 'time', 'freq', 'frequency']:
-    #         ipt = input('Not a valid option - please try again.\n')
-    #
-    #     if ipt in ['1', 'x', 'time']:
-    #         sql_cmd = 'SELECT avg()'
-    #
-    #     if ipt in ['1', '2', 'x', 'y', 'time', 'freq', 'frequency']:
-    #         sql_cmd = f'SELECT avg({tbl_col}) FROM {tbl_name}'
-    #     elif ipt == '*':
-    #         sql_cmd = f'SELECT'
-    #
-    # elif ipt in ['2', 'min']:
-    #     print('option 2: min')
-    # elif ipt in ['3', 'max']:
-    #     print('option 3: max')
-    # elif ipt in ['4', 'sum']:
-    #     print('option 4: sum')
-    # elif ipt in ['5', 'count']:
-    #     print('option 5: count')
+        """ Python 2 """
+        print('Table %s does not exist in current database.'.format(tbl_name))
