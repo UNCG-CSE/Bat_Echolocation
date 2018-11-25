e479609a26418b211c70e784ae1cf529ea6c45bb
Thien Le <t_le@uncg.edu>
2018-11-25 15:53:30 -0500

    Successfully connected to the Live Database

--- src/Web Development/Dash_Django/dashDjango/dashDjango/settings.py
+++ src/Web Development/Dash_Django/dashDjango/dashDjango/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'dashDjango.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'batechodata',
+        'USER': 'batechodata',
+        'PASSWORD': 'batcalls-1',
+        'HOST': 'den1.mysql4.gear.host',
+        'PORT': '',
     }
 }
 
